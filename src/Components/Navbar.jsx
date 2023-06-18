8dc280d05853d60fc9bed2c8ef5e961f03110a62
dhruvin <dhruvin.200410116086@gmail.com>
2023-06-19 00:36:25 +0530

    responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,40 +1,44 @@
-import { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
-import "./Navbar.css";
+
+import './Navbar.css'
+import { useRef } from 'react'
+import { FaTimes, FaBars } from 'react-icons/fa'
+import  { AiFillCaretDown } from 'react-icons/ai'
 import logo from '../assets/logo.webp'
 function Navbar() {
-	const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+    const navRef = useRef();
 
-	return (
-		<header>
-			<img src={logo} alt="" />
-			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">My work</a>
-				<a href="/#">Blog</a>
-				<a href="/#">About me</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-			<button id="hire">
-				Hire Me
-			</button>
-		</header>
-	);
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
+    return (
+        <>
+            <header>
+				<div>
+                <img src={logo} alt="profile pic" />
+				</div>
+                <nav ref={navRef}>
+                    <a href="/#">Home </a>
+                    <a href="/#">about</a>
+                    <a href="/#">works</a>
+                    <a href="/#">contact</a>
+                    <button
+                        className="nav-btn nav-close-btn"
+                        onClick={showNavbar}>
+                        <FaTimes />
+                    </button>
+                </nav>
+                <button
+                    className="nav-btn"
+                    onClick={showNavbar}>
+                  <span>Explore</span> <AiFillCaretDown/>
+                </button>
+                <button className='hire'>
+                    Enterprise Solutions
+                </button>
+            </header>
+        </>
+    )
 }
 
-export default Navbar;
+export default Navbar
